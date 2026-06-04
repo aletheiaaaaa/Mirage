0234cbc3f5a485bdc6a4faf4bedb2e8fd092ace8
aletheiaaaaa <carbon674@gmail.com>
2026-06-04 22:46:54 +0100

    unslopify

--- include/mirage/detail/random.hpp
+++ include/mirage/detail/random.hpp
@@ -2,11 +2,13 @@
 
 #include <Random123/philox.h>
 
+#include <cmath>
 #include <cstddef>
 #include <cstdint>
 #include <eve/module/core.hpp>
 #include <eve/module/math.hpp>
 #include <eve/wide.hpp>
+#include <numeric>
 #include <vector>
 
 namespace mirage::detail {
@@ -53,109 +55,53 @@ kumi::tuple<WideF, WideF> boxmuller_wide(WideU i1, WideU i2) {
 }
 
 template <typename Real>
-class normal_stream {
+class Generator {
   public:
   using RNG = philox_for_t<Real>;
   using word = RNG::ctr_type::value_type;
-  using wide_u = eve::wide<word>;
-  using wide_f = eve::wide<Real>;
-  static constexpr std::size_t W = wide_f::size();
+  using four_u = eve::wide<word, eve::fixed<4>>;
+  using four_f = eve::wide<Real, eve::fixed<4>>;
+  static constexpr size_t W = four_f::size();
 
-  normal_stream(std::uint64_t seed, std::uint64_t stream_id) : seed_(seed), buf1_(W), buf2_(W) {
-    key_ = {{}};
-    ctr_ = {{}};
-    key_[0] = stream_id;
+  Generator(uint64_t seed, uint64_t stream_id) : seed_(seed) {
+    key_ = {{0, stream_id}};
+    ctr_ = {{0, seed}};
   }
 
-  wide_f next() {
-    if (have_spare_) {
-      have_spare_ = false;
-      return spare_;
-    }
-    refill();
-    wide_u i1(buf1_.data());
-    wide_u i2(buf2_.data());
-    auto [z0, z1] = boxmuller_wide<wide_f>(i1, i2);
-    spare_ = z1;
-    have_spare_ = true;
-    return z0;
+  std::pair<four_f, four_f> generate() {
+    auto reg = [&]() {
+      auto r = rng_({{counter_++, seed_}}, key_);
+      return r.v;
+    };
+
+    four_u i1(reg());
+    four_u i2(reg());
+
+    return boxmuller_wide<four_f>(i1, i2);
   }
 
   private:
-  void refill() {
-    std::size_t filled = 0;
-    while (filled < W) {
-      ctr_[0] = counter_++;
-      ctr_[1] = seed_;
-      typename RNG::ctr_type r = rng_(ctr_, key_);
-      for (std::size_t k = 0; k + 1 < r.size() && filled < W; k += 2) {
-        buf1_[filled] = r[k];
-        buf2_[filled] = r[k + 1];
-        ++filled;
-      }
-    }
-  }
-
   RNG rng_;
   typename RNG::key_type key_;
   typename RNG::ctr_type ctr_;
-  std::uint64_t seed_;
-  std::uint64_t counter_ = 0;
-
-  std::vector<word> buf1_, buf2_;
-  wide_f spare_{};
-  bool have_spare_ = false;
+  uint64_t counter_ = 0;
+  uint64_t seed_;
 };
 }  // namespace random
 
 template <typename Real>
-std::vector<Real> random_gaussian(std::size_t n, std::uint64_t seed, std::uint64_t stream_id) {
-  random::normal_stream<Real> ns(seed, stream_id);
-  constexpr std::size_t W = random::normal_stream<Real>::W;
+std::pair<eve::wide<Real>, eve::wide<Real>> random_gaussian(
+  size_t n, uint64_t seed, uint64_t stream_id
+) {
+  random::Generator<Real> gen(seed, stream_id);
 
-  const std::size_t rounded = ((n + W - 1) / W) * W;
-  std::vector<Real> out(rounded);
-
-  for (std::size_t pos = 0; pos < rounded; pos += W) eve::store(ns.next(), out.data() + pos);
-
-  out.resize(n);
-  return out;
+  return gen.generate();
 }
 
 template <typename Real>
-std::vector<Real> random_uniform(
-  std::size_t n_points, std::size_t D, std::uint64_t seed, std::uint64_t stream_id
-) {
-  if (D == 0) return {};
+void normalize(std::vector<Real>& vec, Real eps) {
+  Real norm = std::sqrt(std::inner_product(vec.begin(), vec.end(), vec.begin(), Real(0.0)));
 
-  using wide_f = typename random::normal_stream<Real>::wide_f;
-  constexpr std::size_t W = random::normal_stream<Real>::W;
-
-  random::normal_stream<Real> ns(seed, stream_id);
-
-  const std::size_t rounded = ((n_points + W - 1) / W) * W;
-  std::vector<Real> out(rounded * D);
-
-  std::vector<wide_f> coord(D);
-
-  std::size_t base = 0;
-  for (std::size_t done = 0; done < rounded; done += W) {
-    for (std::size_t d = 0; d < D; ++d) coord[d] = ns.next();
-
-    wide_f sumsq = coord[0] * coord[0];
-    for (std::size_t d = 1; d < D; ++d) sumsq = eve::fma(coord[d], coord[d], sumsq);
-
-    wide_f inv_norm = eve::rsqrt(sumsq);
-
-    for (std::size_t d = 0; d < D; ++d) coord[d] = coord[d] * inv_norm;
-
-    for (std::size_t l = 0; l < W; ++l)
-      for (std::size_t d = 0; d < D; ++d) out[base + (l * D) + d] = coord[d].get(l);
-
-    base += W * D;
-  }
-
-  out.resize(n_points * D);
-  return out;
+  for (auto& x : vec) x /= (norm + Real(1e-8));
 }
 }  // namespace mirage::detail
